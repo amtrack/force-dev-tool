1f559b05063d771b382c9b5337349090b26a4193
Matthias Rolke <mr.amtrack@gmail.com>
2016-05-04 19:15:37 +0200

    replace determineRemote function calls by remotes.get

--- test/remotes/provider/config.js
+++ test/remotes/provider/config.js
@@ -5,7 +5,7 @@ var ConfigRemoteProvider = require("../../../lib/remotes/provider/config");
 var tmp = require('tmp');
 var path = require('path');
 
-describe('Dummy ConfigRemoteProvider', function(){
+describe('ConfigRemoteProvider', function(){
 	describe('#Constructor', function(){
 		it('should initialize a new provider', function(){
 			var tmpobj = tmp.dirSync();
@@ -13,13 +13,41 @@ describe('Dummy ConfigRemoteProvider', function(){
 		});
 	});
 	describe('#add()', function(){
-		it('configRemoteProvider', function(done){
+		it('should add a remote', function(done){
 			var tmpobj = tmp.dirSync();
 			var r = {name: 'foo'};
 			var configRemoteProvider = new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')});
 			configRemoteProvider.add(r, function(err) {
 				assert.deepEqual(err, undefined);
 				done();
+			});
+		});
+		it('should add a remote when config does not exist yet', function(done){
+			var tmpobj = tmp.dirSync();
+			var r = {name: 'foo'};
+			var configRemoteProvider = new ConfigRemoteProvider({path: path.join(tmpobj.name, 'subdir', 'foo.json')});
+			configRemoteProvider.add(r, function(err) {
+				assert.deepEqual(err, undefined);
+				done();
+			});
+		});
+	});
+	describe('#setDefault()', function(){
+		it('configRemoteProvider', function(done){
+			var tmpobj = tmp.dirSync();
+			var r = {name: 'foo'};
+			var r2 = {name: 'foo2'};
+			var configRemoteProvider = new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')});
+			configRemoteProvider.add(r, function(addErr) {
+				assert.deepEqual(addErr, undefined);
+				configRemoteProvider.add(r2, function(add2Err) {
+					assert.deepEqual(add2Err, undefined);
+					configRemoteProvider.setDefault('foo2', function(setDefaultErr){
+						assert.deepEqual(setDefaultErr, undefined);
+						assert.deepEqual(configRemoteProvider.getDefault().name, 'foo2');
+						done();
+					});
+				});
 			});
 		});
 	});
@@ -34,13 +62,32 @@ describe('Dummy ConfigRemoteProvider', function(){
 		});
 	});
 	describe('#get()', function(){
-		it('configRemoteProvider', function(){
+		it('should get a remote', function(){
 			var tmpobj = tmp.dirSync();
 			var configRemoteProvider = new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')});
 			var r = {name: 'foo'};
 			configRemoteProvider.add(r, function(){
-				assert.deepEqual(configRemoteProvider.get('foo'), {name: 'foo'});
+				assert.deepEqual(configRemoteProvider.get('foo').name, 'foo');
 			});
+		});
+		it('should return default remote when no name given', function(done){
+			var tmpobj = tmp.dirSync();
+			var configRemoteProvider = new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')});
+			configRemoteProvider.add({name: 'foo', default: true},
+				function(addErr){
+					assert.deepEqual(addErr, undefined);
+					assert.deepEqual(configRemoteProvider.get().name, 'foo');
+					done();
+				});
+		});
+		it('should fail when remote does not exist', function(){
+			var tmpobj = tmp.dirSync();
+			var configRemoteProvider = new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')});
+			assert.throws(function() {
+				configRemoteProvider.get('nonexistent remote');
+			},
+				/Could not determine remote/
+			);
 		});
 	});
 });
--- test/remotes/provider/multi.js
+++ test/remotes/provider/multi.js
@@ -12,49 +12,93 @@ var env = {
 	'SFDC_PASSWORD': 'default',
 	'SFDC_FOO_USERNAME': 'foo@example.com',
 	'SFDC_FOO_PASSWORD': 'foo',
-	'SFDC_FOO_SERVER_URL': 'https://login.salesforce.com',
-	'SFDC_INVALID_USERNAME': 'invalid@example.com',
-	'SFDC_INVALID_SERVER_URL': 'https://login.salesforce.com'
+	'SFDC_FOO_SERVER_URL': 'https://login.salesforce.com'
 };
 
-var tmpobj = tmp.dirSync();
-var multiProvider = new MultiRemoteProvider({
-	providers: [
-		new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')}),
-		new EnvRemoteProvider({env: env})
-	]
-});
-
-describe('Dummy ConfigRemoteProvider', function(){
+describe('MultiRemoteProvider', function(){
 	describe('#Constructor', function(){
+		var tmpobj = tmp.dirSync();
+		var multiProvider = new MultiRemoteProvider({
+			providers: [
+				new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')}),
+				new EnvRemoteProvider({env: env})
+			]
+		});
 		it('should initialize a new provider', function(){
 			assert.deepEqual(multiProvider.config.providers.length, 2);
 		});
 	});
-	describe('#list()', function(){
-		it('multiProvider', function(){
-			assert.deepEqual(multiProvider.list().length, 3);
-		});
-	});
 	describe('#add()', function(){
+		var tmpobj = tmp.dirSync();
+		var multiProvider = new MultiRemoteProvider({
+			providers: [
+				new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')}),
+				new EnvRemoteProvider({env: env})
+			]
+		});
 		it('multiProvider', function(done){
-			var r = {name: 'foo2'};
+			var r = {name: 'foo'};
 			multiProvider.add(r, function(err) {
 				assert.deepEqual(err, undefined);
+				assert.deepEqual(multiProvider.list().length, 3);
 				done();
 			});
 		});
 	});
+	describe('#setDefault()', function(){
+		var tmpobj = tmp.dirSync();
+		var envWithoutDefault = {
+			'SFDC_FOO_USERNAME': 'foo@example.com',
+			'SFDC_FOO_PASSWORD': 'foo',
+			'SFDC_FOO_SERVER_URL': 'https://login.salesforce.com'
+		};
+		var multiProvider = new MultiRemoteProvider({
+			providers: [
+				new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')}),
+				new EnvRemoteProvider({env: envWithoutDefault})
+			]
+		});
+		it('should set a remote as default', function(done){
+			var r = {name: 'foo'};
+			multiProvider.add(r, function(addErr) {
+				assert.deepEqual(addErr, undefined);
+				multiProvider.setDefault('foo', function(err) {
+					assert.deepEqual(err, undefined);
+					assert.deepEqual(multiProvider.getDefault().name, 'foo');
+					done();
+				});
+			});
+		});
+	});
 	describe('#get()', function(){
+		var tmpobj = tmp.dirSync();
+		var multiProvider = new MultiRemoteProvider({
+			providers: [
+				new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')}),
+				new EnvRemoteProvider({env: env})
+			]
+		});
 		it('multiProvider', function(){
-			assert.deepEqual(multiProvider.get('foo2').name, 'foo2');
+			assert.deepEqual(multiProvider.get('FOO').username, 'foo@example.com');
 		});
 	});
 	describe('#remove()', function(){
+		var tmpobj = tmp.dirSync();
+		var multiProvider = new MultiRemoteProvider({
+			providers: [
+				new ConfigRemoteProvider({path: path.join(tmpobj.name, 'foo.json')}),
+				new EnvRemoteProvider({env: env})
+			]
+		});
 		it('multiProvider', function(done){
-			multiProvider.remove('foo2', function(err) {
-				assert.deepEqual(err, undefined);
-				done();
+			var r = {name: 'foo'};
+			multiProvider.add(r, function(addErr) {
+			assert.deepEqual(addErr, undefined);
+				multiProvider.remove('foo', function(err) {
+					assert.deepEqual(err, undefined);
+					assert.deepEqual(multiProvider.list().length, 2);
+					done();
+				});
 			});
 		});
 	});
