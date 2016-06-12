fc3f3d841abda7125cff2de9bf14d45e4ee8fee0
Matthias Rolke <mr.amtrack@gmail.com>
2016-06-12 18:24:02 +0200

    improve code coverage for PackageXmlParser

--- test/package-xml-parser.js
+++ test/package-xml-parser.js
@@ -5,14 +5,19 @@ var PackageXmlParser = require('../lib/package-xml-parser');
 var testManifests = require('./metadata-parts/manifest');
 
 describe('PackageXmlParser', function() {
-	var packageXml = new PackageXmlParser(testManifests.packageXml);
 	describe('#getApiVersion()', function() {
 		it('should return the api version', function() {
+			var packageXml = new PackageXmlParser(testManifests.packageXml);
 			assert.deepEqual(packageXml.getApiVersion(), '33.0');
+		});
+		it('should return null if api version is not specified', function() {
+			var packageXml = new PackageXmlParser(testManifests.packageXmlWithoutVersion);
+			assert.deepEqual(packageXml.getApiVersion(), null);
 		});
 	});
 	describe('#getComponents()', function() {
 		it('should return the components of the package.xml', function() {
+			var packageXml = new PackageXmlParser(testManifests.packageXml);
 			assert.deepEqual(
 				packageXml.getComponents().map(function(component) {
 					return component.toString()
