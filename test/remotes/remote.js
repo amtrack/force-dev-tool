ebd084f86072d9a7dac0d75e5c1304aee632afd1
Matthias Rolke <mr.amtrack@gmail.com>
2016-06-19 14:05:18 +0200

    improve code coverage for Remote

--- test/remotes/remote.js
+++ test/remotes/remote.js
@@ -2,6 +2,8 @@
 
 var assert = require("assert");
 var Remote = require("../../lib/remotes/remote");
+var sinon = require("sinon");
+var jsforce = require("jsforce");
 
 describe('Remote', function() {
 	describe('#getJSON()', function() {
@@ -13,6 +15,39 @@ describe('Remote', function() {
 					serverUrl: 'https://test.salesforce.com'
 				}
 			);
+		});
+	});
+	describe('#connect()', function() {
+		afterEach(function() {
+			jsforce.Connection.prototype.login.restore();
+		});
+		it('should return a connection for a successful login', function(done) {
+			// login callback with err, userInfo
+			sinon.stub(jsforce.Connection.prototype, 'login').yieldsAsync(
+				null, {
+					id: "asdf",
+					organizationId: "asdf",
+					url: "https://test.salesforce.com"
+				});
+			var r = new Remote('default', 'default@example.com', 'foo');
+			r.connect(function(connErr, conn) {
+				assert(!connErr);
+				assert.deepEqual(conn.loginUrl, "https://test.salesforce.com");
+				done();
+			});
+		});
+		it('should return an error for a failed login', function(done) {
+			// login callback with err, userInfo
+			sinon.stub(jsforce.Connection.prototype, 'login').yieldsAsync(
+				new Error("INVALID_LOGIN: Invalid username, password, security token; or user locked out."),
+				null
+			);
+			var r = new Remote('default', 'default@example.com', 'fooInvalid');
+			r.connect(function(connErr, conn) {
+				assert.deepEqual(connErr, /INVALID_LOGIN/);
+				assert(!conn);
+				done();
+			});
 		});
 	});
 });
