daf930c4f92c618a7c28e00212f60295a329e7c9
Matthias Rolke <mr.amtrack@gmail.com>
2017-01-07 00:38:35 +0100

    cli utils: xargs -0 compatibility
    adds a util function to handle the input of `xargs -0`

--- test/cli/utils.js
+++ test/cli/utils.js
@@ -42,4 +42,24 @@ describe('CliUtils', function() {
 			})
 		});
 	});
+	describe('#handleXargsNull()', function() {
+		context('when passed an Array with newlines', function() {
+			it('returns all items', function() {
+				var input = ['foo\nbar baz\nbazn'];
+				assert.deepEqual(CliUtils.handleXargsNull(input), ['foo', 'bar baz', 'bazn']);
+			});
+		});
+		context('when passed an Array without newlines', function() {
+			it('returns the same items', function() {
+				var input = ['foo', 'bar', 'baz'];
+				assert.deepEqual(CliUtils.handleXargsNull(input), ['foo', 'bar', 'baz']);
+			});
+		});
+		context('when Array contains additional whitespace', function() {
+			it('returns trimmed items', function() {
+				var input = ['foo ', ' bar', '\nbaz', 'bazn\n', '\n'];
+				assert.deepEqual(CliUtils.handleXargsNull(input), ['foo', 'bar', 'baz', 'bazn']);
+			});
+		});
+	});
 });
